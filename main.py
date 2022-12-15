f6deb34d5f33b3a154aab2cc6586a15306680960
supkeopi31 <120625800+supkeopi13@users.noreply.github.com>
2022-12-16 01:31:23 +0900

    ADD background

--- main.py
+++ main.py
@@ -5,25 +5,34 @@ from src.charles import *
 from src.burner import fire, wood
 from src.object import graduation
 
+class background(pygame.sprite.Sprite):
+  def __init__(self):
+    super(background, self).__init__()
+    self.image = pygame.image.load("img/background.png")
+    self.image = pygame.transform.scale(self.image, (500, 500))
+    self.rect = self.image.get_rect()
+  def update(self):
+    pass
+
 def main():
   pygame.init()
   screen = pygame.display.set_mode((500, 500))
   pygame.display.set_caption('Charles\'s law')
 
   charles_init()
-  sprite2 = pygame.sprite.Group([fire(), wood()])
-  sprite3 = pygame.sprite.Group(graduation())
+  sprite2 = pygame.sprite.Group(background())
+  sprite3 = pygame.sprite.Group([fire(), wood(), graduation()])
   spriteGroup = pygame.sprite.Group(particle())
   btnClickedPlus = btnClickedMinus = 0
   
   running = True
   while running:
-    screen.fill((255, 255, 255))
     for event in pygame.event.get():
       if event.type==pygame.QUIT:
         running = False
+    # Screen
+    sprite2.draw(screen)
     # Update
-    sprite2.update()
     sprite3.update()
     spriteGroup.update()
     # Add & Delete particle
@@ -40,7 +49,6 @@ def main():
     # from Other file
     charles_execution(screen)
     # Draw
-    sprite2.draw(screen)
     sprite3.draw(screen)
     spriteGroup.draw(screen)
     pygame.draw.rect(screen, (0, 0, 0), [(width-75, height), (boxHeight, boxWidth)])
